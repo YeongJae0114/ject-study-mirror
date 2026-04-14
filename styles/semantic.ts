0783865e7c3e8a121b9746bbdf5f686a21f76cba
seryoii <seryoii@naver.com>
2026-04-14 22:07:02 +0900

    feat: 추가된 시맨틱 컬러 추가 (4/14)

--- styles/semantic.ts
+++ styles/semantic.ts
@@ -7,25 +7,31 @@ export const semantic = {
     disabled: colors.coolNeutral[300],
     invert: colors.neutral[0],
     error: colors.red[500],
+    brand: colors.primary[500],
+    input: colors.coolNeutral[500],
   },
   bg: {
     default: colors.neutral[0],
     paper: colors.coolNeutral[50],
     accent: colors.coolNeutral[800],
     brand: colors.primary[500],
+    "brand-light": colors.primary[50],
     error: colors.red[500],
     "error-hover": colors.red[600],
     "error-light": colors.red[50],
+    
   },
   btn: {
     "primary-bg": colors.primary[500],
     "primary-hover": colors.primary[600],
     "primary-pressed": colors.primary[700],
     disabled: colors.coolNeutral[100],
+    "secondary-default": colors.coolNeutral[800],
   },
   border: {
     default: colors.coolNeutral[200],
     active: colors.primary[500],
+    "active-light": colors.primary[300],
     error: colors.red[500],
   },
   shadow: {
