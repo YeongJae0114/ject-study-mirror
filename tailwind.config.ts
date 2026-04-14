6e6838c912a47c3c709db3efa3ad613a2a6c3b57
seryoii <seryoii@naver.com>
2026-04-14 19:40:09 +0900

    feat: 디자인 시스템 기반 컬러, 타이포그래피 기본 설정

---
+++ tailwind.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from "tailwindcss";
+import { typography } from "./styles/typography";
+import { colors } from "./styles/color";
+import { semantic } from "./styles/semantic";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Colors
+        primary: colors.primary,
+        neutral: colors.neutral,
+        "cool-neutral": colors.coolNeutral,
+        pink: colors.pink,
+        lime: colors.lime,
+        red: colors.red,
+
+        // Alpha
+        "black-alpha": colors.blackAlpha,
+
+        // Semantic
+        text: semantic.text,
+        bg: semantic.bg,
+        btn: semantic.btn,
+        border: semantic.border,
+      },
+      // Shadow
+      shadow: semantic.shadow,
+      // Typography
+      fontSize: typography.fontSize,
+      fontWeight: typography.fontWeight,
+    },
+  },
+  plugins: [],
+};
+
+export default config;
