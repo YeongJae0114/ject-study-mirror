e16808a5978b71e12c6ac3d72ee3fa89c9693d1a
seryoii <seryoii@naver.com>
2026-05-18 05:27:08 +0900

    feat: art, region 상수 선언 (constants)

---
+++ src/constants/art.ts
@@ -0,0 +1,14 @@
+export const ART_TYPES = [
+  {
+    label: "벽면형",
+    description: "액자, 패널 등을 활용해 벽면에 거는 형태",
+  },
+  {
+    label: "테이블형",
+    description: "테이블, 선반 등에 작품을 배치하는 형태",
+  },
+  {
+    label: "공간점유형",
+    description: "바닥, 특정 구역 등에 작품을 설치하는 형태",
+  },
+];
