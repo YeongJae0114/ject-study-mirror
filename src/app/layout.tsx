1bdce56abafae9c561de6b635da28564f4c755c8
seryoii <seryoii@naver.com>
2026-06-10 18:04:17 +0900

    chore: clarity 스크립트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import ScrollManager from "@/components/common/ScrollManager";
 import Providers from "./providers";
 
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "ReFit",
@@ -43,6 +44,18 @@ export default function RootLayout({
           <ScrollManager />
         </Suspense>
         <Navbar />
+        <Script id="clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);
+              t.async=1;
+              t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];
+              y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+          `}
+        </Script>
       </body>
     </html>
   );
